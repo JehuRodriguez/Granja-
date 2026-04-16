1baf0ad4fa3d94989c16cd3d605cff201664c811
Jesus Rodriguez Echenique <j.ehu14@hotmail.com>
2026-04-16 11:05:57 -0500

    Agregando clase Granja

---
+++ Granja/Granja.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Granja
+{
+     class Granja
+    {
+        public int dinero = 100;
+        public int espacio = 5;
+
+        public List<Planta> plantas = new List<Planta>();
+        public List<Animal> animales = new List<Animal>();
+
+        public void ComprarPlanta(Planta planta)
+        {
+            plantas.Add(planta);
+            Console.WriteLine("Compraste planta");
+        }
+
+        public void ComprarAnimal(Animal animal)
+        {
+            animales.Add(animal);
+            Console.WriteLine("Compraste animal");
+        }
+
+        public void PasarTurno()
+        {
+            Console.WriteLine(" Nuevo Día ");
+
+            foreach (var planta in plantas)
+            {
+                planta.Crecer();
+            }
+
+            foreach (var animal in animales)
+            {
+                animal.PasarDia();
+                animal.Producir();
+            }
+        }
+
+    }
+}
