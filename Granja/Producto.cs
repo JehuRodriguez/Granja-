6f150ffb5aa8f8bc39a47636171f49cf45e8b59b
Jesus Rodriguez Echenique <j.ehu14@hotmail.com>
2026-04-16 10:33:30 -0500

    Creando la clase Producto

---
+++ Granja/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Granja
+{
+    abstract class Producto
+    {
+        public string nombre;
+        public int precioVenta;
+
+        public abstract void Vender();
+
+    }
+}
