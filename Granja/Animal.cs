8b23bcc5de4aea2baff736aac23cf47de7e8a4ef
Jesus Rodriguez Echenique <j.ehu14@hotmail.com>
2026-04-16 10:58:31 -0500

    Creando la clase Animal:IProducible

---
+++ Granja/Animal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Granja
+{
+     class Animal:IProducible
+    {
+        public string nombre;
+        public int diasParaProducir;
+        public int diasActuales = 0;
+
+        public void PasarDia()
+        {
+            diasActuales++;
+        }
+
+        public void Producir()
+        {
+            if (diasActuales >= diasParaProducir)
+            {
+                Console.WriteLine(nombre + " produjo algo");
+                diasActuales = 0;
+            }
+        }
+    }
+}
