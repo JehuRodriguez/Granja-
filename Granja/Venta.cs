399430cfe122f1e81b5492dd62d9cd11c5b827f9
Jesus Rodriguez Echenique <j.ehu14@hotmail.com>
2026-04-16 11:27:40 -0500

    Agregando clase Venta

---
+++ Granja/Venta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Granja
+{
+     class Venta
+    {
+        public string nombreProducto;
+        public int precio;
+        public int diasRestantes;
+    }
+}
