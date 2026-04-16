e5f88c5a2e2c5e180afd6d0fcb260f5f428b8009
Jesus Rodriguez Echenique <j.ehu14@hotmail.com>
2026-04-16 10:42:53 -0500

    Creando clase Planta:Producto

---
+++ Granja/Planta.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Granja
+{
+    class Planta:Producto
+    {
+        public int diasParaCrecer;
+        public int diasActuales = 0;
+
+        public void Crecer()
+        {
+            diasActuales++;
+        }
+
+        public bool ListaParaCosechar()
+        {
+            return diasActuales >= diasParaCrecer;
+        }
+
+        public override void Vender()
+        {
+            Console.WriteLine("Vendiste la planta: " + nombre);
+        }
+    }
+}
